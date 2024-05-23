2d8375309153f4dcf120c3322bd9c1ad47f557d5
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2024-05-23 14:15:24 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js";
+import { connectToMongoDB } from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server running on port ${PORT}`);
+});
